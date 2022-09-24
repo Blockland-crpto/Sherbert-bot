ca96853a8e6c8961ed45830aa7641e5b684c06b2
Blockland-crpto <herbgamerwow@gmail.com>
2022-09-24 18:35:29 -0400

    added .catches to the interaction.replys of ban

--- commands/ban.js
+++ commands/ban.js
@@ -16,29 +16,35 @@ module.exports = {
 			return 1;
 		}
 		else if (interaction.user.id === user.id) {
-			await interaction.reply({ content: 'Were sorry, but you cannot ban yourself, please try again', ephemeral: true });
+			await interaction.reply({ content: 'Were sorry, but you cannot ban yourself, please try again', ephemeral: true })
+				.catch(console.error);
 			return 1;
 		}
 		else if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-			await interaction.reply({ content: 'Were sorry, but you cannot use the ban command, you do not have the permissions, please try again', ephemeral: true });
+			await interaction.reply({ content: 'Were sorry, but you cannot use the ban command, you do not have the permissions, please try again', ephemeral: true })
+				.catch(console.error);
 			return 1;
 		}
 		else if (client.user.id === user.id) {
-			await interaction.reply({ content: 'Were sorry, but you cannot ban SherbertBot, please try again', ephemeral: true });
+			await interaction.reply({ content: 'Were sorry, but you cannot ban SherbertBot, please try again', ephemeral: true })
+				.catch(console.error);
 			return 1;
 		}
 		else if (userm === null) {
-			await interaction.reply({ content: 'Were sorry, but you cannot ban a user thats not part of this guild, please try again', ephemeral: true });
+			await interaction.reply({ content: 'Were sorry, but you cannot ban a user thats not part of this guild, please try again', ephemeral: true })
+				.catch(console.error);
 			return 1;
 		}
 		else if (!userm.bannable) {
-			await interaction.reply({ content: `Were sorry, but you cannot ban ${user}, they have permissions that are greater then SherbertBot, please try again`, ephemeral: true });
+			await interaction.reply({ content: `Were sorry, but you cannot ban ${user}, they have permissions that are greater then SherbertBot, please try again`, ephemeral: true })
+				.catch(console.error);
 			return 1;
 		}
 		else {
 
 			interaction.guild.members.ban(user);
-			await interaction.reply({ content: `${user} has been successfully banned from ${interaction.guild.name}`, ephemeral: true });
+			await interaction.reply({ content: `${user} has been successfully banned from ${interaction.guild.name}`, ephemeral: true })
+				.catch(console.error);
 		}
 
 		client.on('shardError', error => {
